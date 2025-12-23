eb207e426f2aabfd776ba76aedc7187ae8836dd6
zoeyang0 <lwvgnrs495@hotmail.com>
2025-12-23 17:40:57 +0800

    Deploy script now targets VeilPredictionMarket

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedPrediction = await deploy("VeilPredictionMarket", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`VeilPredictionMarket contract: `, deployedPrediction.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_prediction_market"; // id required to prevent reexecution
+func.tags = ["VeilPredictionMarket"];
